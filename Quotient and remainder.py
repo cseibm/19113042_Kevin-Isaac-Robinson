7e694d0cd6d8dbcc2fc3f25a4e1372b8a6c68ea3
cseibm <68462471+cseibm@users.noreply.github.com>
2020-07-18 17:56:32 +0530

    Create Quotient and remainder.py

---
+++ Quotient and remainder.py
@@ -0,0 +1,3 @@
+print("To divide two numbers")
+a, b= int(input("Enter the numerator... ")), int(input("Enter the denominator... "))
+print("The quotient is", int(a/b), "\nThe remainder is", int(a%b))

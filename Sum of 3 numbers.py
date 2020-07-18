8bae4e0f6dd94c736587be968126932be961a20e
cseibm <68462471+cseibm@users.noreply.github.com>
2020-07-18 17:48:58 +0530

    Create Sum of 3 numbers.py

---
+++ Sum of 3 numbers.py
@@ -0,0 +1,2 @@
+a, b, c = int(input("Enter the 1st number... ")), int(input("Enter the 2nd number... ")), int(input("Enter the 3rd number... "))
+print("The sum of the three entered numbers is", a+b+c)

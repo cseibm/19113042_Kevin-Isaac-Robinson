fa9b77974a5d52c747fbd9dd0d33eab2c3815512
cseibm <68462471+cseibm@users.noreply.github.com>
2020-07-19 21:53:09 +0530

    Update Ratio of two numbers.py

--- Ratio of two numbers.py
+++ Ratio of two numbers.py
@@ -3,8 +3,8 @@ b = int(input("Enter the second number\t"))
 i=1
 while (i <= a and i<= b):
     if (a % i == 0 and b % i == 0):
-        gdc=i
+        gcf=i
     i+=1
-a = int(a / gdc)
-b = int(b / gdc)
+a = int(a / gcf)
+b = int(b / gcf)
 print("Ratio of the two entered numbers is\n", a, ":", b)

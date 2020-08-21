eb7ff58e3d59b9acb9d3f2b6bc65fa31bf9c32f7
cseibm <68462471+cseibm@users.noreply.github.com>
2020-08-21 21:22:32 +0530

    Create GUVI Question 2.py

---
+++ GUVI Question 2.py
@@ -0,0 +1,26 @@
+list = input()
+sum = 0
+length = 0
+MyKey = {}
+HitsKey = {}
+multiply = 2
+for i in list:
+    string = str(i)
+    length = len(string)
+    sum = 0
+    for j in string[0: : 2]:
+        sum += int(j)
+    while sum in MyKey:
+        sum *= multiply
+        multiply += 1
+    MyKey[sum] = i
+    sum = 0
+    multiply = 2
+    for j in string[1: :2]:
+        sum += int(j)
+    while sum in HitsKey:
+        sum *= multiply
+        multiply += 1
+    HitsKey[sum] = i
+print(MyKey)
+print(HitsKey)
